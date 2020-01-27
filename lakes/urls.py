fea9c28cf5f6c9d5b11301ae61f9c32a3349ae6e
Alan Donohoe <donohoe.al@gmail.com>
2020-01-27 10:53:10 -0800

    Add a lakes map based on openlayes and another based on leaflet

--- lakes/urls.py
+++ lakes/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, LeafletView, lakes_view, contours_view
 
 app_name = 'lakes'
 
 urlpatterns = [
-    path('',HomePageView.as_view(), name='home')
+    path('',HomePageView.as_view(), name='home'),
+    path('leaflet/', LeafletView.as_view(), name='leaflet'),
+    path('lakes/', lakes_view, name='lakes'),
+    path('contours/<int:lake_id>/', contours_view, name='contours'),
 ]
