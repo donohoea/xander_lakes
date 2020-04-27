564201c6400207267fecbdfcda707ea642fae5aa
Alan Donohoe <donohoe.al@gmail.com>
2020-04-26 18:27:21 -0700

    Fix manage.py

--- backend/manage.py
+++ backend/manage.py
@@ -8,12 +8,13 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xander.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
+    except ImportError as exc:donohoe_al@xander-lakes:~/xander_lakes/frontend$ sudo apt-get install
+
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
+        ) from except
     execute_from_command_line(sys.argv)
 
 
